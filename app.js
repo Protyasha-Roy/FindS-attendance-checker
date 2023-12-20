678b0c678758489ceec33a45c92c469106603547
Protyasha-Roy <protyasharoy369@gmail.com>
2023-12-20 15:36:47 +0600

    connected to the server

--- app.js
+++ app.js
@@ -1,56 +1,126 @@
-let savedStudents = JSON.parse(localStorage.getItem('students')) || [];
+// let savedStudents = JSON.parse(localStorage.getItem('students')) || [];
 
 
-document.getElementById('entryBtn').addEventListener('click', (event) => {
+// document.getElementById('entryBtn').addEventListener('click', (event) => {
+//     event.preventDefault();
+//     const studentName = document.getElementById('studentName').value.trim();
+//     const studentRoll = document.getElementById('studentRoll').value.trim();
+
+
+//     if (studentName === '' || studentRoll === '') {
+//         document.getElementById('entryMessage').innerText = 'Please fill both fields before submitting.';
+//         return;
+//     }
+
+//     const existingStudent = savedStudents.find(student => student.roll === studentRoll);
+
+//     if (existingStudent) {
+//         document.getElementById('entryMessage').innerText = 'A student with this roll already exists.';
+//         return;
+//     }
+
+//     const newStudent = { name: studentName, roll: studentRoll };
+
+//     savedStudents.push(newStudent);
+
+    
+//     localStorage.setItem('students', JSON.stringify(savedStudents));
+
+    
+//     document.getElementById('entryMessage').innerText = 'New entry submitted: ' + studentName + ' (Roll: ' + studentRoll + ')';
+   
+//     document.getElementById('studentName').value = '';
+//     document.getElementById('studentRoll').value = '';
+// });
+
+// document.getElementById('checkBtn').addEventListener('click', () => {
+//     const searchRollInput = document.getElementById('searchRoll').value.trim();
+//     const searchRolls = searchRollInput.split(',').map(roll => roll.trim());
+
+//     if (searchRollInput === '') {
+//         document.getElementById('checkMessage').innerText = 'Please fill the textarea before checking.';
+//         return;
+//     }
+
+//     const mismatchedRolls = searchRolls.filter(searchRoll => {
+//         return !savedStudents.some(student => student.roll == searchRoll);
+//     });
+
+//     if (mismatchedRolls.length > 0) {
+//         document.getElementById('checkMessage').innerText = 'Mismatched Rolls: ' + mismatchedRolls.join(', ');
+//     } else {
+//         document.getElementById('checkMessage').innerText = 'All Rolls Matched!';
+//     }
+
+//     document.getElementById('searchRoll').value = '';
+// });
+const entryBtn = document.getElementById('entryBtn');
+const checkBtn = document.getElementById('checkBtn');
+const searchRollField = document.getElementById('searchRoll');
+const entryMessage = document.getElementById('entryMessage');
+const checkMessage = document.getElementById('checkMessage');
+
+const userId = localStorage.getItem('userId');
+
+entryBtn.addEventListener('click', async (event) => {
     event.preventDefault();
     const studentName = document.getElementById('studentName').value.trim();
     const studentRoll = document.getElementById('studentRoll').value.trim();
 
-
     if (studentName === '' || studentRoll === '') {
-        document.getElementById('entryMessage').innerText = 'Please fill both fields before submitting.';
+        entryMessage.innerText = 'Please fill both fields before submitting.';
         return;
     }
 
-    const existingStudent = savedStudents.find(student => student.roll === studentRoll);
+    try {
+        const response = await fetch(`http://localhost:10000/add-student`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name: studentName, roll: studentRoll, userId: userId }),
+        });
 
-    if (existingStudent) {
-        document.getElementById('entryMessage').innerText = 'A student with this roll already exists.';
-        return;
+        const data = await response.json();
+        entryMessage.innerText = data.message;
+    } catch (error) {
+        console.error('Error adding new student:', error);
+        entryMessage.innerText = 'Error adding new student.';
     }
 
-    const newStudent = { name: studentName, roll: studentRoll };
-
-    savedStudents.push(newStudent);
-
-    
-    localStorage.setItem('students', JSON.stringify(savedStudents));
-
-    
-    document.getElementById('entryMessage').innerText = 'New entry submitted: ' + studentName + ' (Roll: ' + studentRoll + ')';
-   
     document.getElementById('studentName').value = '';
     document.getElementById('studentRoll').value = '';
 });
 
-document.getElementById('checkBtn').addEventListener('click', () => {
-    const searchRollInput = document.getElementById('searchRoll').value.trim();
-    const searchRolls = searchRollInput.split(',').map(roll => roll.trim());
+checkBtn.addEventListener('click', async (event) => {
+    event.preventDefault();
+    const searchRollInput = searchRollField.value.trim();
 
     if (searchRollInput === '') {
-        document.getElementById('checkMessage').innerText = 'Please fill the textarea before checking.';
+        checkMessage.innerText = 'Please fill the textarea before checking.';
         return;
     }
 
-    const mismatchedRolls = searchRolls.filter(searchRoll => {
-        return !savedStudents.some(student => student.roll == searchRoll);
-    });
+    try {
+        const response = await fetch('http://localhost:10000/check-rolls', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ rolls: searchRollInput.split(',').map(roll => roll.trim()), userId }),
+        });
 
-    if (mismatchedRolls.length > 0) {
-        document.getElementById('checkMessage').innerText = 'Mismatched Rolls: ' + mismatchedRolls.join(', ');
-    } else {
-        document.getElementById('checkMessage').innerText = 'All Rolls Matched!';
+        const data = await response.json();
+
+        if (data.mismatchedRolls.length > 0) {
+            checkMessage.innerText = 'Mismatched Rolls: ' + data.mismatchedRolls.join(', ');
+        } else {
+            checkMessage.innerText = 'All Rolls Matched!';
+        }
+    } catch (error) {
+        console.error('Error checking rolls:', error);
+        checkMessage.innerText = 'Error checking rolls.';
     }
 
-    document.getElementById('searchRoll').value = '';
+    searchRollField.value = '';
 });
